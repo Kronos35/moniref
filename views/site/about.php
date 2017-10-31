5a247a86363109e6c972426c9b57f6d723b88a85
Kronos35 <xxchronos35xx@gmail.com>
2017-10-31 12:47:31 -0600

    pushazo loco man

--- views/site/about.php
+++ views/site/about.php
@@ -10,7 +10,7 @@ $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<<<<<<< HEAD
+
 
     <?php 
         echo Html::jsFile('@web/js/Chart.bundle.js'); 
@@ -37,17 +37,33 @@ $this->params['breadcrumbs'][] = $this->title;
             GROUP BY
                 a.idApliance
         ')->queryAll();
+    //print_r($consumptionWatts);
+
+    $dataset = array();
+    
+	foreach ($consumptionWatts as $subarray) {
+		$id = 0;
+		$arrvalue = 0;
+		foreach ($subarray as $key => $value) {
+			if($key = "idApliance"){
+				$id = $value;
+			}
+			else{
+				$arrvalue = $value;
+			}
+		}
+		$dataset["app-id-".$id] = $arrvalue;
+	}
+	print_r($dataset);
+    
 	// clase de charts //
 	// requiere llamar esto: "use app\assets\Charts;" //
 
-	// el constructor pide charts (array(clave=>valor)) de datos, $string parametros (aquí va el col-md-5 o esas weas) y finalmente un bool random o no random (true = random)
-	$charts = new Charts(["data1"=>1,"data2"=>9, "potato"=>50, "data3"=>13,"data4"=>8, "potato2"=>25],"col-md-4",false);
-	//se usa el metodo render para mostrar el chart
+	// el constructor de charts pide $string(id del chart), (array(clave=>valor)) de datos, $string parametros (aquí va el col-md-5 o esas weas) y finalmente un bool random o no random (true = random)
+    $charts = new Charts("charid",$dataset,"col-md-3",true);
 	$charts->render();
 ?>
 
-=======
->>>>>>> ab0cef846409dfb63aed3ff03d2582480bf0daa1
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
@@ -58,9 +74,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     	<code><?= __FILE__ ?></code>
     </div>
-<<<<<<< HEAD
 </div>
-=======
 </div>
 <div class="row">
 	
@@ -70,7 +84,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	// requiere llamar esto: "use app\assets\Charts;" //
 
 	// el constructor de charts pide $string(id del chart), (array(clave=>valor)) de datos, $string parametros (aquí va el col-md-5 o esas weas) y finalmente un bool random o no random (true = random)
-	$charts = new Charts("charid",["data1"=>1,"data2"=>9, "potato"=>50, "data3"=>13,"data4"=>8, "potato2"=>25],"col-md-3",true);
+	$charts = new Charts("charid",$dataset,"col-md-3",false);
 	// metodo que permite cambiar el tipo de grafica, si no se usa por defecto es el de dona
 	$charts->setChartType($charts->type[0]);
 	// metodo que permite ordenar los datos de menor a mayor
@@ -79,17 +93,17 @@ $this->params['breadcrumbs'][] = $this->title;
 	$charts->render();
 
 	// Este es un ejemplo de otra chart en la misma pantalla
-	$charts2 = new Charts("charid2",["potato"=>99, "data3"=>180,"data4"=>40, "potato2"=>13],"col-md-3",false);
+	$charts2 = new Charts("charid2",$dataset,"col-md-3",false);
 	$charts2->setChartType($charts2->type[1]);
 	$charts2->render();
 
-	$charts3 = new Charts("charid3",["data2"=>11, "potato"=>21, "data3"=>7,"data4"=>30, "potato2"=>44],"col-md-3",true);
+	$charts3 = new Charts("charid3",$dataset,"col-md-3",true);
 	$charts3->setChartType($charts3->type[2]);
 	// este metodo permite cambiar el nombre a la grafica
 	$charts3->setChartTitle("Mi graficota");
 	$charts3->render();
 
-	$charts4 = new Charts("charid4",["data2"=>11, "potato"=>21, "data3"=>7,"data4"=>30, "potato2"=>44],"col-md-3",true);
+	$charts4 = new Charts("charid4",$dataset,"col-md-3",true);
 	$charts4->setChartType($charts4->type[3]);
 	$charts4->render();
 ?>
@@ -97,15 +111,15 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="row">
 	<?php
-		$charts5 = new Charts("charid5",["data2"=>11, "potato"=>21, "data3"=>7,"data4"=>30, "potato2"=>44],"col-md-3",true);
+		$charts5 = new Charts("charid5",$dataset,"col-md-3",true);
 		$charts5->setChartType($charts5->type[4]);
 		$charts5->render();
 
-		$charts6 = new Charts("charid6",["data2"=>11, "potato"=>21, "data3"=>7,"data4"=>30, "potato2"=>44],"col-md-3",true);
+		$charts6 = new Charts("charid6",$dataset,"col-md-3",true);
 		$charts6->setChartType($charts5->type[5]);
 		$charts6->render();
 	?>
 	
 </div>
 
->>>>>>> ab0cef846409dfb63aed3ff03d2582480bf0daa1
+
