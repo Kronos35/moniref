980eb3e97a5075e2997579b4c20514c849176267
marcom333 <dev.marco.medina@gmail.com>
2017-11-06 19:24:18 -0700

    UPDATE

--- config/web.php
+++ config/web.php
@@ -48,8 +48,8 @@ $config = [
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-            ),*/
-        ],
+            ),
+        ],*/
     ],
     'params' => $params,
 ];
